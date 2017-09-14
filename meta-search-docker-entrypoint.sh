66b4c738386da3c67706c13b5a93d9df8d80f7cb
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-09-14 11:29:45 -0700

    using shell script to add interrupt ability

---
+++ meta-search-docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+search_engines="/var/www/html/search-engines.txt"
+
+echo -n > "$search_engines"
+
+while [ -n "$1" ]; do
+  echo "$1" > "$search_engines"
+  echo "Adding search engine \"$1\"."
+  shift
+done
+
+echo "Starting apache in foreground."
+docker-php-entrypoint apache2-foreground &
+apache_pid="$!"
+
+kill_apache() {
+  kill "$apache_pid"
+}
+#trap 'kill_apache' INT
+wait "$apache_pid"
